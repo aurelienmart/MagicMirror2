1bcd9a7b97fe1dce7c2173a89d1c4c0451953551
Hango Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-10-03 16:07:55 +0300

    Add files via upload

--- modules/default/weatherforecast/weatherforecast.js
+++ modules/default/weatherforecast/weatherforecast.js
@@ -162,7 +162,7 @@ Module.register("weatherforecast", {
 				var rainCell = document.createElement("td");
 				if (isNaN(forecast.rain)) {
 					rainCell.className = "align-right shade";
-					rainCell.innerHTML = this.translate("No rain");
+					rainCell.innerHTML = "0.0 mm &nbsp;<i class=\"wi wi-raindrop lightskyblue\"></i>&nbsp;" // this.translate("No rain");
 				} else if (!isNaN(forecast.snow)) {
 					if(config.units !== "imperial") {
 						rainCell.innerHTML = parseFloat(forecast.snow).toFixed(1).replace(".", this.config.decimalSymbol) + " mm <i class=\"wi wi-snowflake-cold lightblue\"></i>";
@@ -171,9 +171,9 @@ Module.register("weatherforecast", {
 					}
 				} else {
 					if (config.units !== "imperial") {
-						rainCell.innerHTML = parseFloat(forecast.rain).toFixed(1).replace(".", this.config.decimalSymbol) + " mm <i class=\"wi wi-umbrella skyblue\"></i>";
+						rainCell.innerHTML = parseFloat(forecast.rain).toFixed(1).replace(".", this.config.decimalSymbol) + " mm &nbsp;<i class=\"wi wi-umbrella skyblue\"></i>&nbsp;";
 					} else {
-						rainCell.innerHTML = (parseFloat(forecast.rain) / 25.4).toFixed(2).replace(".", this.config.decimalSymbol) + " in <i class=\"wi wi-umbrella skyblue\"></i>";
+						rainCell.innerHTML = (parseFloat(forecast.rain) / 25.4).toFixed(2).replace(".", this.config.decimalSymbol) + " in &nbsp;<i class=\"wi wi-umbrella skyblue\"></i>&nbsp;";
 					}
 				} 
 				rainCell.className = "align-right bright rain";
