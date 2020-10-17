c820a92e7382e2ddc6693411d5334bfa4ac4d67b
Horia Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2020-10-17 20:38:38 +0300

    Add files via upload

--- modules/rssfeed/rssfeed.js
+++ modules/rssfeed/rssfeed.js
@@ -9,26 +9,7 @@
  */
 Module.register("rssfeed", {
 
-    defaults: {
-        lengthDescription: 600,
-        fetchNews: 600 * 1000,
-        feeds: {
-//          "NewsIn"    : "https://newsin.ro/feed",
-//          "Discovery" : "https://discovery.ro/feed/",
-//          "Agerpress" : "https://www.agerpres.ro/home.rss",
-//          "Digi24"    : "https://m.digi24.ro/rss",
-            "ProTV"     : "https://rss.stirileprotv.ro",
-            "Mediafax"  : "https://www.mediafax.ro/rss",
-            "HotNews"   : "https://www.hotnews.ro/rss",
-            "News.ro"   : "https://www.news.ro/rss",
-            "MainNews"  : "https://mainnews.ro/feed",
-            "Ziare.com" : "https://www.ziare.com/rss/12h.xml"
-        },
-        feedMaxAge: {
-            days: 0,
-            hours: 12
-        },
-    },
+    defaults: {},
 
     getScripts: function() {
         return ["jquery.js"];
@@ -61,9 +42,9 @@ Module.register("rssfeed", {
     },
 
     newsfeed: function() {
-        var feedURLs = this.defaults.feeds;
-        var feedMaxAge = this.defaults.feedMaxAge;
-        var lengthDescription = this.defaults.lengthDescription;
+        var feedURLs = this.config.feeds;
+        var feedMaxAge = this.config.feedMaxAge;
+        var lengthDescription = this.config.lengthDescription;
         
         $.fn.updateWithTextForce = function(text, speed, force) {
             var news = $("<div/>").html(text);
@@ -77,9 +58,9 @@ Module.register("rssfeed", {
         };
         if(typeof feedURLs == "undefined") {
             if(typeof feed == "undefined")
-                var feedURLs;
+                feedURLs;
             else
-                var feedURLs = {"News" : feed};
+                feedURLs = {"News" : feed};
         }
         $(document).ready(function($) {
             var news = [];
@@ -119,25 +100,25 @@ Module.register("rssfeed", {
                 });
             }
             function addStoryForFeed(stories, oldestDate, story) {
-        		var pubDate = moment(story.find("pubDate").text(), "HH:mm:ss");
+        		var pubDate = moment(story.find("pubDate").text(), "HH:mm:ss Z");
         		var title = story.find("title").text().replace("VIDEO","").replace("FOTO","");
         		var desc = story.find("description").text().substring(0,600).trim().replace("div>","span>").replace("<br","<base").replace(/&#8230;|]]>/,"...").replace(/--|__/,"");
         		var counter = "<span class=\"count\">" + (title.length + desc.length) + "</span>";
         		var update = "<span class=\"update\">(" + counter + "/" + moment().format("HH:mm") + ")</span>";
         		if(oldestDate.diff(pubDate) < 0){
-        			stories.push("<span class=\"date\"> - " + pubDate.toNow() + ":</span><br><span class=\"title\">" + title + "</span> &bull; <span class=\"desc\">" + desc + "</span>");
+        			stories.push("<span class=\"date\"> - " + pubDate.toNow().replace("peste","în urmă cu") + ":</span><br><span class=\"title\">" + title + "</span> &bull; <span class=\"desc\">" + desc + "</span>");
                 }
             }(
             function showNews() {
                 var initialFeed = newsFeedIndex;
-                if(news.length == 0) {
+                if(news.length === 0) {
                     return;
                 }
                 for(var i=0; i < news.length+1; i++) {
                     var newsFeed = news[newsFeedIndex];
                     if(newsFeed === undefined)
                         continue;
-                    if(newsFeed.length == 0) {
+                    if(newsFeed.length === 0) {
                         if(++newsFeedIndex == news.length)
                             newsFeedIndex = 0;
                         newsStoryIndex = 0;
@@ -151,18 +132,18 @@ Module.register("rssfeed", {
                         }
                     }
                 }
-                if(news[newsFeedIndex].length == 0) {
+                if(news[newsFeedIndex].length === 0) {
                     setTimeout(showNews, 2000);
                     return;
                 }
-                var i = 0;
+                i = 0;
                 for(var key in feedURLs) {
                     if(i == newsFeedIndex)
                         break;
                     i++;
                 }
                 $(".source").updateWithTextForce("<i class=\"fa fa-rss-square\"></i> " + key, 2000, true);
-                var newsFeed = news[newsFeedIndex];
+                newsFeed = news[newsFeedIndex];
                 newsStory = newsFeed[newsStoryIndex];
                 newsStoryIndex++;
                 var nextTimeout = 1000;
