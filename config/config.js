f2e7ea64ca72bb55837867f106142e67a844ba68
Horia Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2021-04-28 16:30:28 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -318,7 +318,7 @@ var config = {
 			config: {
 				maxNumberOfDays: 4,
 				forecastEndpoint: "/forecast",
-				fallBack: false,
+				fallBack: true,
 				fullday: "HH [h]",
 				initialLoadDelay: 2000,
 				showRainAmount: true,
@@ -336,7 +336,7 @@ var config = {
 				maxNumberOfDays: 9,
 //				locationID: false,
 //				forecastEndpoint: "onecall",
-				fallBack: true,
+				fallBack: false,
 				fullday: "ddd",
 				initialLoadDelay: 3000,
 				showRainAmount: true,
