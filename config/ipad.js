3bd32d73604ea055eed3295f416d444ef7160d83
Răzvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2021-09-25 10:29:43 +0300

    Update ipad.js

--- config/ipad.js
+++ config/ipad.js
@@ -32,7 +32,7 @@ var config = {
 	minVersion: "2.14.0",
 	serverOnly: true,
 	DeepMerge: true,
-	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // "DEBUG", 
+	logLevel: [], // "DEBUG", "INFO", "LOG", "WARN", "ERROR"
 
 	modules: [
 		{
